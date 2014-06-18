d2d9fcdace71e4cd175484489bcce57444caa5ea
Paul M. Rodriguez <pmr@ruricolist.com>
2014-06-18 14:47:30 -0500

    Refactor.

--- cl-textcat.lisp
+++ cl-textcat.lisp
@@ -8,7 +8,17 @@
   (defvar *base*
     #.(fad:pathname-directory-pathname (or *compile-file-truename* *load-truename*))))
 
+(defparameter *ngram-limit* 400
+  "The default ngram limit for comparisons.")
+
+(defparameter *sample-size* 20000
+  "The maximum sample size, in characters.")
+
+(defparameter *cutoff* 1.05
+  "The default cutoff for plausible guesses.")
+
 (defun load-model (file)
+  "Load a language model from FILE into a hash table."
   (let ((lines
           (lines
            (read-file-into-string
@@ -31,11 +41,34 @@
            collect (cons language (load-model model))))
    t))
 
-(defun classify (input &key (ngram-limit 400)
+(defun input-model (input &key (sample-size *sample-size*)
+                               remove-singletons)
+  (check-type input string)
+  (let* ((sample
+           (if (or (not sample-size)
+                   (> sample-size (length input)))
+               input
+               (nsubseq input 0 sample-size)))
+         (model (create-lm sample
+                           :remove-singletons
+                           (if (numberp remove-singletons)
+                               (> (length sample) remove-singletons)
+                               remove-singletons))))
+    (map-into model #'car model)))
+
+(defun language-score (unknown model &optional (ngram-limit *ngram-limit*))
+  (loop for i from 0
+        for ngram in unknown
+        for match = (gethash ngram model)
+        if match
+          sum (abs (- match i))
+        else sum ngram-limit))
+
+(defun classify (input &key (ngram-limit *ngram-limit*)
                             (languages *language-models*)
                             remove-singletons
-                            (cutoff 1.05)
-                            (sample-size 20000))
+                            (cutoff *cutoff*)
+                            (sample-size *sample-size*))
   "Return the language of INPUT.
 
 LANGUAGES is the alist of language models to use; default is
@@ -51,36 +84,24 @@ long texts, but should not be used with short texts.
 
 If a guess is unlikelier than than CUTOFF times the previous guess, it
 is discarded."
-  (let* ((sample
-           (if (or (not sample-size)
-                   (> sample-size (length input)))
-               input
-               (nsubseq input 0 sample-size)))
-         (unknown
-           (let ((lm (create-lm sample
-                                :remove-singletons
-                                (if (numberp remove-singletons)
-                                    (> (length sample) remove-singletons)
-                                    remove-singletons))))
-             (map-into lm #'car lm)))
+  (let* ((unknown (input-model input
+                               :sample-size sample-size
+                               :remove-singletons remove-singletons))
          (distances
            (loop for (language . model) in languages
                  collect (cons language
-                               (loop for i from 0
-                                     for ngram in unknown
-                                     for match = (gethash ngram model)
-                                     if match
-                                       sum (abs (- match i))
-                                     else sum ngram-limit))))
-         (results (sort distances #'< :key #'cdr)))
-    (let ((results
-            (destructuring-bind ((best . score) . rest) results
-              (cons (alpha-2 best)
-                    (let ((base (* score cutoff)))
-                      (loop for (lang . score) in rest
-                            while (< score base)
-                            collect (alpha-2 lang)))))))
-      (values (first results) (rest results)))))
+                               (language-score unknown model
+                                               ngram-limit))))
+         (sorted-distances (sort distances #'< :key #'cdr))
+         (results
+           (destructuring-bind ((best . score) . rest)
+               sorted-distances
+             (cons (alpha-2 best)
+                   (let ((base (* score cutoff)))
+                     (loop for (lang . score) in rest
+                           while (< score base)
+                           collect (alpha-2 lang)))))))
+    (values (first results) (rest results))))
 
 (defun update-lm (lm input)
   (prog1 lm
@@ -150,7 +171,7 @@ This implements the reduced n-gram approach from Hornik et al."
    input
    :remove-empty-subseqs t))
 
-(defun finalize-lm (lm &key (ngram-limit 400) remove-singletons)
+(defun finalize-lm (lm &key (ngram-limit *ngram-limit*) remove-singletons)
   (when remove-singletons
     (maphash (lambda (k v)
                (when (= v 1)
@@ -159,7 +180,7 @@ This implements the reduced n-gram approach from Hornik et al."
   (let ((alist (hash-table-alist lm)))
     (firstn ngram-limit (sort alist #'> :key #'cdr))))
 
-(defun create-lm (input &key remove-singletons (ngram-limit 400))
+(defun create-lm (input &key remove-singletons (ngram-limit *ngram-limit*))
   (finalize-lm (update-lm (dict) input)
                :remove-singletons remove-singletons
                :ngram-limit ngram-limit))
